2aced3a8c76d21d3522e23daf78d36de0f610e3c
Jossic <jossic.lapierre@gmail.com>
2022-01-10 10:32:57 +0100

    initial commit

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils';
+
+describe('Utils test suite', () => {
+	it('first test', () => {
+		const abc = Utils.toUpperCase('abc');
+		console.log('test work');
+	});
+});
